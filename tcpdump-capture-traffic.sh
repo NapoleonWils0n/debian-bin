0c79c60ca87b558758bf6505900d64fee02f0c1d
Daniel J Wilcox <danieljwilcox@gmail.com>
2016-11-29 15:50:56 +0000

    tcpdump capture

--- tcpdump-capture-traffic.sh
+++ tcpdump-capture-traffic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo tcpdump -s 0 host 192.168.1.3 -i ens9 -w ens9-$(date +"%H-%M-%m-%d-%y").pcap
+sudo tcpdump -s 0 host 192.168.1.3 -i enp0s26f7u1u3 -w tcpdump-$(date +"%H-%M-%m-%d-%y").pcap
