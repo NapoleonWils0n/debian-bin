97483e125814f934f6866fceeb0ce365562d6f3f
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-02-11 20:13:33 +0000

    removed old scripts

--- android-backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-adb backup -apk -shared -all -f /home/djwilcox/Documents/backup/android-backup/nexus-4/nexus-4-cmod-$(date +"%d-%m-%y").ab
--- backup-home.sh
+++ backup-home.sh
@@ -2,4 +2,4 @@
 
 # rsync home backup
 
-rsync --delete -azvv /home/djwilcox root@192.168.1.25::NetBackup/djwilcox/
+rsync --delete -azvv --exclude={"/home/djwilcox/docker/*","/home/djwilcox/libvirt/*"} /home/djwilcox root@192.168.1.25::NetBackup/djwilcox/
